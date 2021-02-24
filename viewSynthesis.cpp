c01aab005be0e040c7a64f1db0a3de8b2d8773a7
YuYue525 <58425611+YuYue525@users.noreply.github.com>
2021-02-25 00:02:19 +0800

    Add files via upload

--- viewSynthesis.cpp
+++ viewSynthesis.cpp
@@ -100,7 +100,77 @@ int main(int argc, char** argv)
             }
             else
             {
+                int x, y;
+                double a, b, X, Y;
+                X = Vx + (Vz*(70*c-17885))/1024.0/targetFocalLen;//the position of the intersection
+                Y = Vy + (Vz*(70*r-17885))/1024.0/targetFocalLen;
+                              
+                x = (int)(X + 120)/Baseline;
+                y = (int)(120 - Y)/Baseline;
+
+                a = (Vx + 120 - (x * Baseline))/(double)Baseline;// alpha and beta of the position of the intersection
+                b = (120 - Vy - (y * Baseline))/(double)Baseline;
                 
+                int ci, ri;
+                double u, v, alpha, beta;
+                u = (double)(70*c-17885))/1024.0*100.0/targetFocalLen+17.5;// the position of the pixel
+                v = (double)(70*r-17885))/1024.0*100.0/targetFocalLen+17.5;
+
+                ci = (int)((double)(u - 35.0/1024.0))/((double)(35.0/512.0));
+                ri = (int)((double)(v - 35.0/1024.0))/((double)(35.0/512.0));
+                
+                alpha = (u - (ci*35.0/512.0+35.0/1024.0))/(double)(35.0/512.0);
+                beta = 1-(v - (ri*35.0/512.0+35.0/1024.0))/(double)(35.0/512.0);
+                
+                unsigned char red1, green1, blue1;// four rays of neighbour viewpoints
+                unsigned char red2, green2, blue2;
+                unsigned char red3, green3, blue3;
+                unsigned char red4, green4, blue4;
+                
+                for(int i = 0; i<2;i++)
+                {
+                    for(int j = 0; j<2; j++)
+                    {
+                        unsigned char r1, g1, b1;
+                        unsigned char r2, g2, b2;
+                        unsigned char r3, g3, b3;
+                        unsigned char r4, g4, b4;
+                        
+                        viewImageList[9 * (y+i) + (x+j)].getColor(ci, ri+1, r1, g1, b1);
+                        viewImageList[9 * (y+i) + (x+j)].getColor(ci+1, ri+1, r2, g2, b2);
+                        viewImageList[9 * (y+i) + (x+j)].getColor(ci, ri, r3, g3, b3);
+                        viewImageList[9 * (y+i) + (x+j)].getColor(ci+1, ri, r4, g4, b4);
+                        
+                        if(i == 0 && j ==0)
+                        {
+                            red1 = (double)((1-beta)*((1-alpha)*r1+alpha*r2)+beta*((1-alpha)*r3+alpha*r4));
+                            green1 = (double)((1-beta)*((1-alpha)*g1+alpha*g2)+beta*((1-alpha)*g3+alpha*g4));
+                            blue1 = (double)((1-beta)*((1-alpha)*b1+alpha*b2)+beta*((1-alpha)*b3+alpha*b4));
+                        }
+                        else if(i == 0 && j == 1)
+                        {
+                            red2 = (double)((1-beta)*((1-alpha)*r1+alpha*r2)+beta*((1-alpha)*r3+alpha*r4));
+                            green2 = (double)((1-beta)*((1-alpha)*g1+alpha*g2)+beta*((1-alpha)*g3+alpha*g4));
+                            blue2 = (double)((1-beta)*((1-alpha)*b1+alpha*b2)+beta*((1-alpha)*b3+alpha*b4));
+                        }
+                        else if(i ==1 && j == 0)
+                        {
+                            red3 = (double)((1-beta)*((1-alpha)*r1+alpha*r2)+beta*((1-alpha)*r3+alpha*r4));
+                            green3 = (double)((1-beta)*((1-alpha)*g1+alpha*g2)+beta*((1-alpha)*g3+alpha*g4));
+                            blue3 = (double)((1-beta)*((1-alpha)*b1+alpha*b2)+beta*((1-alpha)*b3+alpha*b4));
+                        }
+                        else
+                        {
+                            red4 = (double)((1-beta)*((1-alpha)*r1+alpha*r2)+beta*((1-alpha)*r3+alpha*r4));
+                            green4 = (double)((1-beta)*((1-alpha)*g1+alpha*g2)+beta*((1-alpha)*g3+alpha*g4));
+                            blue4 = (double)((1-beta)*((1-alpha)*b1+alpha*b2)+beta*((1-alpha)*b3+alpha*b4));
+                        }
+                    }
+                }
+                
+                rayRGB.x = (double)((1-b)*((1-a)*red1+a*red2)+b*((1-a)*red3+a*red4));
+                rayRGB.y = (double)((1-b)*((1-a)*green1+a*green2)+b*((1-a)*green3+a*green4));
+                rayRGB.z = (double)((1-b)*((1-a)*blue1+a*blue2)+b*((1-a)*blue3+a*blue4));
             }
             
 			//! record the resampled pixel value
